1bc320d9580423ef388bb48b98977a6b19d9140f
AlaaM <121508011+alaaty29@users.noreply.github.com>
2025-01-03 15:23:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-document.getElementById('calculateBtn').addEventListener('click',calculateLoan);
+
+document.getElementById("incrementBtn").addEventListener("click",incrementCounter);
+
+document.getElementById("decrementBtn").addEventListener("click",decremetnCounter);
+
+document.getElementById("resetBtn").addEventListener("click",resetCounter);
+
+let counterDisplay = document.getElementById("counterDisplay");
+let counterValue = 0;
+
+function updateCounterDisplay(){
+    counterDisplay.textContent = counterValue;
+}
 
 
-function calculateLoan(){
 
-
-const loanAmount = parseFloat(document.getElementById("loanAmountInput").value);
-const interestRate = parseFloat(document.getElementById("interestRateInput").value);
-const loanTerm = parseFloat(document.getElementById("loanTermInput").value);
-
-
-    if(isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)){
-        alert("Please Enter valid Numbers for all the Fields");
-    }
-
-    const monthlyInterest = interestRate /100 / 12;
-    const totalPayments = loanTerm;
-    const monthlyPayment = (loanAmount * monthlyInterest)/(1-Math.pow(1 + monthlyInterest, -totalPayments));
-    const totalInterst = (monthlyPayment * totalPayments) - loanAmount;
-
-
-    displayResult(monthlyPayment,totalInterst);
+function incrementCounter(){
+    counterValue++;
+    updateCounterDisplay();
 
 }
 
-function displayResult(monthlyPayment, totalInterst){
+function decremetnCounter(){
+    if(counterValue>0){
+        counterValue--;
+        updateCounterDisplay();
+    }
+    
+    
+}
 
-    const resultDiv = document.getElementById('result');
+function resetCounter(){
+    counterValue = 0;
+    updateCounterDisplay();
 
-    resultDiv.innerHTML = `
-        <p><strong>Monthly Payment: ${monthlyPayment.toFixed(2)}</p></strong>
-        <p><strong>Total Interest: ${totalInterst.toFixed(2)}</p></strong>
-    `;
 }
