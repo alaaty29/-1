b356053852a2728bab4ba308490a2e524de2b8b2
AlaaM <121508011+alaaty29@users.noreply.github.com>
2025-01-03 15:23:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
+function isPlaindrome(str){
 
-document.getElementById("incrementBtn").addEventListener("click",incrementCounter);
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g,'');
+    console.log(cleanStr);
 
-document.getElementById("decrementBtn").addEventListener("click",decremetnCounter);
+    const reversedStr = cleanStr.split('').reverse().join('');
+    console.log(reversedStr);
 
-document.getElementById("resetBtn").addEventListener("click",resetCounter);
-
-let counterDisplay = document.getElementById("counterDisplay");
-let counterValue = 0;
-
-function updateCounterDisplay(){
-    counterDisplay.textContent = counterValue;
+    return cleanStr === reversedStr;
 }
 
 
+function plaindromeChecker(){
 
-function incrementCounter(){
-    counterValue++;
-    updateCounterDisplay();
+    const inputText = document.getElementById("inputText");
+    const result = document.getElementById("result");
 
-}
-
-function decremetnCounter(){
-    if(counterValue>0){
-        counterValue--;
-        updateCounterDisplay();
+    if(isPlaindrome(inputText.value)){
+        result.textContent = `"${inputText.value}" is a Palindrome`;
+    }else{
+        result.textContent = `"${inputText.value}" is Not a Palindrome`;
     }
-    
-    
-}
 
-function resetCounter(){
-    counterValue = 0;
-    updateCounterDisplay();
+    result.classList.add('fadeIn');
+    inputText.value = "";
 
 }
+
+
+document.getElementById('checkButton').addEventListener("click",plaindromeChecker);
