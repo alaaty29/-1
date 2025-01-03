b3178ea285d8aa86c756c154b0e76604492be490
AlaaM <121508011+alaaty29@users.noreply.github.com>
2025-01-03 15:25:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,15 @@
-let dateContainer = document.querySelector(".date-container");
-let hoursContainer = document.querySelector(".hours");
-let minutesContainer = document.querySelector(".minutes");
-let secondsContainer = document.querySelector(".seconds");
+function detectChar(){
 
+    const input = document.getElementById("inputType").value;
 
-const weekdays =[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wendesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+    if(input){
 
-const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
+        const unicodeValue = input.charCodeAt(0);
+        const result = `The Unicode value of "${input}" is ${unicodeValue}`;
+        document.getElementById('result').textContent = result;  
 
-
-function formatTime(time){
-    return time < 10 ? "0" + time : time;  //  0 : 45 : 10  -> 00 : 45 : 10 
-}
-
-function updateClock(){
-
-
-    const today = new Date();
-   // console.log(today);
-    let date = today.getDate();
-   // console.log(date)
-    let day = weekdays[today.getDay()];
-    let month = monthNames[today.getMonth()];
-
-    // console.log(day,month)
-    // console.log(today.getDay(),today.getMonth());
-
-    let hours = formatTime(today.getHours());
-    let minutes = formatTime(today.getMinutes());
-    let seconds = formatTime(today.getSeconds());
-
-
-     dateContainer.innerHTML = ` <p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;   
-
-     hoursContainer.textContent = hours + ":";
-     minutesContainer.textContent = minutes + ":";
-     secondsContainer.textContent = seconds;
+    }else{
+        document.getElementById('result').textContent = 'Please Enter a Character';
+    }
 
 }
-
-setInterval(updateClock,1000);
